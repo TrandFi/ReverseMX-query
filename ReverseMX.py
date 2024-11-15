46b63614f0497e5766d970f4dc367c23d20e2ddf
Кирилл Каюков <kirillsport13112004@gmail.com>
2024-11-15 10:32:21 +1000

    Update ReverseMX

--- ReverseMX.py
+++ ReverseMX.py
@@ -1,13 +1,15 @@
-import ipaddress
-import socket
-import requests  # работа с HTTP-запросами к веб-страницам
+import ipaddress #  Обработка IP адресов
+import socket #  Работа с серверами
+import requests  # Работа с HTTP-запросами к веб-страницам
 from prettytable import PrettyTable #  Библиотека для работы с таблицами
 
-API_KEY_FROM_VIEWDNS = "13e89d74db1aa14da7246b65b2fc110ecf60a8a0"
+API_KEY_FROM_VIEWDNS = "13e89d74db1aa14da7246b65b2fc110ecf60a8a0" #  API ключ с сайта ViewDNS
 
-def is_valid_ip(ip):
+def is_valid_ip(ip): #  Функция провери корректности (существования) IP адреса
     try:
-        ipaddress.ip_address(ip)
+        ipaddress.ip_address(ip) #  Проверка IP-адреса. 
+        #Если адрес не является допустимым адресом IPv4, то возникает ошибка AddressValueError (поскольку дання функция возвращает тип IPv4)
+
         return True
     except ValueError:
         return False
@@ -21,36 +23,35 @@ def ip_lookup(domain_name):
         return None
 
 
-def get_websites_on_server(ip):
-    url = f"https://api.viewdns.info/reverseip/?host={ip}&apikey={API_KEY_FROM_VIEWDNS}&output=json"
+def get_domains_from_viewdns(ip):
+    url = f"https://api.viewdns.info/reverseip/?host={ip}&apikey={API_KEY_FROM_VIEWDNS}&output=json" #  Формируем запрос на сайт ViewDNS, указывая IP нудного нам хоста и наш API ключ
     response = requests.get(url)  #  Отправляем запрос на указанный ресурс
     if response.ok:  #  В случае положительного соединения с хостом
         data = response.json()  #  Преобразуем ответ в json формат
         #  Данная функция автоматически преобразует объект ответа от хоста в словарь
 
-        if "response" in data and "domains" in data["response"]:  #  Проверяем наличие ключей "response" и "domains" в словаре ответа хоста
-            websites = data["response"]["domains"]  #  Записываем в переменную значение ключа response[domains]. 
+        if "response" in data and "domains" in data["response"]:  #  Проверяем наличие ключей "response" и "domains" в словаре data
+            domainlist = data["response"]["domains"]  #  Записываем в переменную значение ключа response[domains]. 
             #  В результате переменная websites будет иметь тип списка list, каждым элементом которого является словарь dictionaty
             
-            return websites
+            return domainlist
     return [] #  Возвращаем пустой список, если по указанному домену сервера не было найдено поддоменов
 
 def main():
     table = PrettyTable(["№","Domain name","Last resolved"])
     i = 0
-    domain = input()
+    domain = input("Input domain server name for search:\n>")
     ip_domain = ip_lookup(domain)
     if not is_valid_ip(ip_domain):
         print(f"\n[error] Invalid IP address for: {domain}")     
     else:
         print(f"\n[success] Domain server: {domain}, IP: {ip_domain}\n")
-        websites = get_websites_on_server(ip_domain)
-        if websites != []:
-            print(len(websites),"subdomains were found on this server:")         
-            for website in websites:
+        domainlist = get_domains_from_viewdns(ip_domain)
+        if domainlist != []:
+            print(len(domainlist),"subdomains were found on this server:")         
+            for d in domainlist:
                 i+=1  
-                table.add_row([i, website["name"], website["last_resolved"]])                                         
-                #print(f"[+] {website["last_resolved"]}")
+                table.add_row([i, d["name"], d["last_resolved"]])                                                       
             print(table)
         else:
             print("[-] No other websites found on the same server.")   
